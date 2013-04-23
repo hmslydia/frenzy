7df0c2c008ad1dcc94315f1c24cbc60924df928e
felicia0 <felicia0@cs.washington.edu>
2013-04-23 14:10:16 -0700

    hierarchy size fix

---
+++ saved/pilotTestResults.js
@@ -0,0 +1,529 @@
+{
+  "tweets": {
+    "nature50": {
+      "time": 50,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/241004.jpg'  width='95%'  display='block' >",
+      "id": "nature50",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature51": {
+      "time": 51,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/241048.jpg'  width='95%'  display='block' >",
+      "id": "nature51",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature52": {
+      "time": 52,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/253027.jpg'  width='95%'  display='block' >",
+      "id": "nature52",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature53": {
+      "time": 53,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/253055.jpg'  width='95%'  display='block' >",
+      "id": "nature53",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature54": {
+      "time": 54,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/260058.jpg'  width='95%'  display='block' >",
+      "id": "nature54",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature55": {
+      "time": 55,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/271035.jpg'  width='95%'  display='block' >",
+      "id": "nature55",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature56": {
+      "time": 56,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/285079.jpg'  width='95%'  display='block' >",
+      "id": "nature56",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature57": {
+      "time": 57,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/291000.jpg'  width='95%'  display='block' >",
+      "id": "nature57",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature58": {
+      "time": 58,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/295087.jpg'  width='95%'  display='block' >",
+      "id": "nature58",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature59": {
+      "time": 59,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/296007.jpg'  width='95%'  display='block' >",
+      "id": "nature59",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature60": {
+      "time": 60,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/296059.jpg'  width='95%'  display='block' >",
+      "id": "nature60",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature61": {
+      "time": 61,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/299086.jpg'  width='95%'  display='block' >",
+      "id": "nature61",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature62": {
+      "time": 62,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/300091.jpg'  width='95%'  display='block' >",
+      "id": "nature62",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature63": {
+      "time": 63,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/302008.jpg'  width='95%'  display='block' >",
+      "id": "nature63",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature64": {
+      "time": 64,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/304034.jpg'  width='95%'  display='block' >",
+      "id": "nature64",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature65": {
+      "time": 65,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/304074.jpg'  width='95%'  display='block' >",
+      "id": "nature65",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature66": {
+      "time": 66,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/306005.jpg'  width='95%'  display='block' >",
+      "id": "nature66",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature67": {
+      "time": 67,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/3096.jpg'  width='95%'  display='block' >",
+      "id": "nature67",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature68": {
+      "time": 68,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/33039.jpg'  width='95%'  display='block' >",
+      "id": "nature68",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "nature69": {
+      "time": 69,
+      "html": "<img src='http://abstract.cs.washington.edu/~hmslydia/cascadeImages/nature/images/351093.jpg'  width='95%'  display='block' >",
+      "id": "nature69",
+      "parent": "none",
+      "creator": "nature"
+    },
+    "felicia00": {
+      "time": 1366153169181,
+      "html": "#transportation",
+      "id": "felicia00",
+      "parent": "nature69",
+      "creator": "felicia0"
+    },
+    "felicia01": {
+      "time": 1366153177956,
+      "html": "#transportation",
+      "id": "felicia01",
+      "parent": "nature67",
+      "creator": "felicia0"
+    },
+    "hmslydia0": {
+      "time": 1366153178979,
+      "html": "#underwater",
+      "id": "hmslydia0",
+      "parent": "nature66",
+      "creator": "hmslydia"
+    },
+    "felicia02": {
+      "time": 1366153182876,
+      "html": "#animals",
+      "id": "felicia02",
+      "parent": "nature66",
+      "creator": "felicia0"
+    },
+    "felicia03": {
+      "time": 1366153186073,
+      "html": "#animals",
+      "id": "felicia03",
+      "parent": "nature65",
+      "creator": "felicia0"
+    },
+    "hmslydia1": {
+      "time": 1366153195387,
+      "html": "#people",
+      "id": "hmslydia1",
+      "parent": "nature63",
+      "creator": "hmslydia"
+    },
+    "felicia04": {
+      "time": 1366153201423,
+      "html": "#animals",
+      "id": "felicia04",
+      "parent": "nature64",
+      "creator": "felicia0"
+    },
+    "felicia05": {
+      "time": 1366153214390,
+      "html": "#animals",
+      "id": "felicia05",
+      "parent": "nature52",
+      "creator": "felicia0"
+    },
+    "felicia06": {
+      "time": 1366153218997,
+      "html": "#animals",
+      "id": "felicia06",
+      "parent": "nature53",
+      "creator": "felicia0"
+    },
+    "felicia07": {
+      "time": 1366153227571,
+      "html": "#people",
+      "id": "felicia07",
+      "parent": "nature56",
+      "creator": "felicia0"
+    },
+    "hmslydia2": {
+      "time": 1366153227903,
+      "html": "#people",
+      "id": "hmslydia2",
+      "parent": "nature55",
+      "creator": "hmslydia"
+    },
+    "felicia08": {
+      "time": 1366153234986,
+      "html": "#people",
+      "id": "felicia08",
+      "parent": "nature55",
+      "creator": "felicia0"
+    },
+    "felicia09": {
+      "time": 1366153254189,
+      "html": "#people",
+      "id": "felicia09",
+      "parent": "nature62",
+      "creator": "felicia0"
+    },
+    "hmslydia3": {
+      "time": 1366153257908,
+      "html": "#people",
+      "id": "hmslydia3",
+      "parent": "nature62",
+      "creator": "hmslydia"
+    },
+    "felicia010": {
+      "time": 1366153260620,
+      "html": "#people\n#animals",
+      "id": "felicia010",
+      "parent": "nature61",
+      "creator": "felicia0"
+    },
+    "felicia011": {
+      "time": 1366153262636,
+      "html": "#animals",
+      "id": "felicia011",
+      "parent": "nature60",
+      "creator": "felicia0"
+    },
+    "hmslydia4": {
+      "time": 1366153264295,
+      "html": "#animals",
+      "id": "hmslydia4",
+      "parent": "nature60",
+      "creator": "hmslydia"
+    },
+    "felicia012": {
+      "time": 1366153266705,
+      "html": "#animals",
+      "id": "felicia012",
+      "parent": "nature59",
+      "creator": "felicia0"
+    },
+    "felicia013": {
+      "time": 1366153273409,
+      "html": "#animals",
+      "id": "felicia013",
+      "parent": "nature57",
+      "creator": "felicia0"
+    },
+    "hmslydia5": {
+      "time": 1366153274389,
+      "html": "#scenery",
+      "id": "hmslydia5",
+      "parent": "nature58",
+      "creator": "hmslydia"
+    },
+    "felicia014": {
+      "time": 1366153285471,
+      "html": "#landscape\n#scenery",
+      "id": "felicia014",
+      "parent": "nature51",
+      "creator": "felicia0"
+    },
+    "felicia015": {
+      "time": 1366153293641,
+      "html": "#mountains",
+      "id": "felicia015",
+      "parent": "nature50",
+      "creator": "felicia0"
+    },
+    "hmslydia6": {
+      "time": 1366153295512,
+      "html": "#pyramids",
+      "id": "hmslydia6",
+      "parent": "nature61",
+      "creator": "hmslydia"
+    },
+    "felicia016": {
+      "time": 1366153296639,
+      "html": "#mountains",
+      "id": "felicia016",
+      "parent": "nature51",
+      "creator": "felicia0"
+    },
+    "felicia017": {
+      "time": 1366153303068,
+      "html": "#scenery",
+      "id": "felicia017",
+      "parent": "nature50",
+      "creator": "felicia0"
+    },
+    "felicia018": {
+      "time": 1366153316609,
+      "html": "should we call these mountains :)",
+      "id": "felicia018",
+      "parent": "nature54",
+      "creator": "felicia0"
+    },
+    "felicia019": {
+      "time": 1366153321794,
+      "html": "#scenery",
+      "id": "felicia019",
+      "parent": "nature58",
+      "creator": "felicia0"
+    }
+  },
+  "streams": {
+    "uist": {
+      "id": "uist",
+      "counter": 8
+    },
+    "viz": {
+      "id": "viz",
+      "counter": 8
+    }
+  },
+  "users": {
+    "hmslydia": {
+      "id": "hmslydia",
+      "counter": 7
+    },
+    "felicia0": {
+      "id": "felicia0",
+      "counter": 20
+    }
+  },
+  "likes": [],
+  "requiredHashtags": [
+    "#"
+  ],
+  "conversation": [
+    {
+      "user": "hmslydia",
+      "comment": "Hello World!",
+      "time": 1366153131383
+    },
+    {
+      "user": "katlyn",
+      "comment": "I like lots of cats. Cats are the bestest of all",
+      "time": 1366153131383
+    },
+    {
+      "user": "hmslydia",
+      "comment": "Poooooop",
+      "time": 1366153131383
+    }
+  ],
+  "currentLocations": [
+    {
+      "user": "felicia0",
+      "id": "nature58"
+    },
+    {
+      "user": "hmslydia",
+      "id": "nature61"
+    }
+  ],
+  "history": {
+    "locations": [
+      {
+        "user": "felicia0",
+        "id": "nature69",
+        "time": 1366153164851
+      },
+      {
+        "user": "felicia0",
+        "id": "nature67",
+        "time": 1366153173614
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature66",
+        "time": 1366153173714
+      },
+      {
+        "user": "felicia0",
+        "id": "nature66",
+        "time": 1366153179911
+      },
+      {
+        "user": "felicia0",
+        "id": "nature65",
+        "time": 1366153183808
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature65",
+        "time": 1366153185586
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature63",
+        "time": 1366153191770
+      },
+      {
+        "user": "felicia0",
+        "id": "nature64",
+        "time": 1366153196526
+      },
+      {
+        "user": "felicia0",
+        "id": "nature52",
+        "time": 1366153211239
+      },
+      {
+        "user": "felicia0",
+        "id": "nature53",
+        "time": 1366153216470
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature55",
+        "time": 1366153225050
+      },
+      {
+        "user": "felicia0",
+        "id": "nature56",
+        "time": 1366153225464
+      },
+      {
+        "user": "felicia0",
+        "id": "nature55",
+        "time": 1366153232643
+      },
+      {
+        "user": "felicia0",
+        "id": "nature62",
+        "time": 1366153251699
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature62",
+        "time": 1366153253774
+      },
+      {
+        "user": "felicia0",
+        "id": "nature61",
+        "time": 1366153255467
+      },
+      {
+        "user": "felicia0",
+        "id": "nature60",
+        "time": 1366153260633
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature60",
+        "time": 1366153260931
+      },
+      {
+        "user": "felicia0",
+        "id": "nature59",
+        "time": 1366153264319
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature58",
+        "time": 1366153268023
+      },
+      {
+        "user": "felicia0",
+        "id": "nature57",
+        "time": 1366153269164
+      },
+      {
+        "user": "felicia0",
+        "id": "nature51",
+        "time": 1366153277711
+      },
+      {
+        "user": "felicia0",
+        "id": "nature50",
+        "time": 1366153289706
+      },
+      {
+        "user": "hmslydia",
+        "id": "nature61",
+        "time": 1366153290946
+      },
+      {
+        "user": "felicia0",
+        "id": "nature51",
+        "time": 1366153294419
+      },
+      {
+        "user": "felicia0",
+        "id": "nature50",
+        "time": 1366153297468
+      },
+      {
+        "user": "felicia0",
+        "id": "nature54",
+        "time": 1366153311457
+      },
+      {
+        "user": "felicia0",
+        "id": "nature58",
+        "time": 1366153319282
+      }
+    ],
+    "clicks": []
+  }
+}
